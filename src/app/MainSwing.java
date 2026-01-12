2a4b9bbb23bbf73a713ec28846cd9d9785527586
Csamull <samueltrusdaly3@gmail.com>
2026-01-12 19:26:48 -0300

    Implementação do Swing do Main para o view

---
+++ src/app/MainSwing.java
@@ -0,0 +1,15 @@
+package src.app;
+
+import javax.swing.SwingUtilities;
+import src.view.TelaPrincipal;
+
+public class MainSwing {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            new TelaPrincipal().setVisible(true);
+        });
+
+    }
+}
